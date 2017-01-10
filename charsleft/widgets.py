e1809b80769699ae82b696b536990efec30a0a0a
Jonas Ohrstrom <ohrstrom@gmail.com>
2017-01-10 15:14:00 +0100

    fix force_text for Python 2/3 versions

--- charsleft/widgets.py
+++ charsleft/widgets.py
@@ -2,11 +2,15 @@
 from __future__ import unicode_literals
 from django import forms
 from django.template.loader import get_template
-from django.utils.encoding import force_unicode
 from django.utils.safestring import mark_safe
 from django.contrib.staticfiles.storage import staticfiles_storage
 
 from django.forms.utils import flatatt
+
+try:
+    from django.utils.encoding import force_text
+except ImportError:
+    from django.utils.encoding import force_text as force_text
 
 
 class MediaMixin(object):
@@ -27,14 +31,14 @@ class CharsLeftCharFieldWidget(forms.TextInput, MediaMixin):
         final_attrs = self.build_attrs(attrs, type=self.input_type, name=name)
 
         if value != '':
-            final_attrs['value'] = force_unicode(self._format_value(value))
+            final_attrs['value'] = force_text(self._format_value(value))
 
         maxlength = final_attrs.get('maxlength')
 
         if not maxlength:
             return mark_safe(u'<input%s />' % flatatt(final_attrs))
 
-        current = force_unicode(int(maxlength) - len(value))
+        current = force_text(int(maxlength) - len(value))
 
         return mark_safe(self.tpl.render({
             'attrs': flatatt(final_attrs),
@@ -60,7 +64,7 @@ class CharsLeftTextFieldWidget(forms.TextInput, MediaMixin):
 
         final_attrs['cols'] = 80
         final_attrs['rows'] = int(int(maxlength)/100) + 1
-        current = force_unicode(int(maxlength) - len(value))
+        current = force_text(int(maxlength) - len(value))
 
         return mark_safe(self.tpl.render({
             'attrs': flatatt(final_attrs),
